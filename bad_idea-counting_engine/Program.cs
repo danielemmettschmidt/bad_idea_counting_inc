46d77106a5ac2eec3bbc9db86485f1f06d4939e7
Daniel <demmettschmidt@gmail.com>
2020-05-22 14:28:56 -0400

    initial c# code

---
+++ bad_idea-counting_engine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Threading;
+
+namespace Bad_Idea_Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counting_Engine counting_engine = new Counting_Engine(10, 8, false);
+
+        }
+    }
+
+}
